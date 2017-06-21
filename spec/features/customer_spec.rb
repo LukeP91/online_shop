72c6f61e1088eeb1db16557108962b1c477a02a0
Luke Pawlik <lukepawlik@gmail.com>
2017-06-21 22:29:30 +0200

    add scenario for basic happy path in which customer makes an order

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -23,4 +23,33 @@ describe "Customer actions", :type => :feature do
     end
     expect(page).to have_css("p", text: "There are no items in your cart.")
   end
+
+  scenario "Customer can make an order" do
+    visit "/"
+    click_link "Add"
+    click_link "checkout_button"
+    within("#new_order") do
+      fill_in 'order_first_name', with: 'Test'
+      fill_in 'order_last_name', with: 'User'
+      fill_in 'order_email', with: 'user@test.com'
+      fill_in 'order_street', with: 'Street'
+      fill_in 'order_city', with: 'City'
+      fill_in 'order_country', with: 'Country'
+      fill_in 'order_zip_code', with: '00-000'
+      fill_in 'order_house_number', with: '1'
+      fill_in 'order_apartment_number', with: '1'
+      click_button 'Checkout'
+    end  
+    expect(page).to have_css("p", text: "There are no items in your cart.")
+    expect(Order.count).to eq 1
+    expect(Order.first).to have_attributes(first_name: "Test", 
+                                           last_name: "User",
+                                           email: "user@test.com",
+                                           street: "Street",
+                                           city: "City", 
+                                           country: "Country",
+                                           zip_code: "00-000",
+                                           house_number: "1",
+                                           apartment_number: 1)
+  end 
 end
